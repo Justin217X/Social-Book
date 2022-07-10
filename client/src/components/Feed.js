b8ff85047f3b17b306c8af14d61466d4541a3d6a
Justin Hued <huedjustin@gmail.com>
2022-07-10 00:40:16 -0400

    Added Feed

---
+++ client/src/components/Feed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Feed.css';
+import PostPrompt from './PostPrompt';
+
+const Feed = () => {
+  return (
+    <div className='feed'>
+        <PostPrompt />
+    </div>
+  )
+}
+
+export default Feed;
