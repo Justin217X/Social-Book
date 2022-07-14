b744b8abb62559addedbe981613ee5e5904d0ab7
Justin Hued <huedjustin@gmail.com>
2022-07-14 16:40:15 -0400

    created routes

---
+++ client/src/components/RouteSwitch.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "../App";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+
+
+const RouteSwitch = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                    <Route path='/' element={<SignUp />}/>
+                    <Route path='/login' element={<SignIn />}/>
+                    <Route path='/SocialBook' element={<App />}/>
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+
+export default RouteSwitch;
